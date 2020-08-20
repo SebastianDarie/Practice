326c6d2ce4915909c65d1dc2fd12cddeb70218f1
SebastianDarie <s.d.01.2002@gmail.com>
2020-08-20 17:56:02 +0300

    dropdown menu

---
+++ dropdown/script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('.drop')
+const contact = document.querySelector('#contact')
+
+function reveal(e) {
+	e.currentTarget.parentNode.childNodes[3].classList.toggle('show')
+}
+
+btn.addEventListener('click', reveal)
+contact.addEventListener('click', reveal)
