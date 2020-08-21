c81b18bf57bc6cade5a81b75803bd341f074f61c
SebastianDarie <s.d.01.2002@gmail.com>
2020-08-21 13:30:51 +0300

    added mobile menu

--- dropdown/script.js
+++ dropdown/script.js
@@ -2,7 +2,7 @@ const btn = document.querySelector('.drop')
 const contact = document.querySelector('#contact')
 
 function reveal(e) {
-	e.currentTarget.parentNode.childNodes[3].classList.toggle('show')
+    e.currentTarget.parentNode.childNodes[3].classList.toggle('show')
 }
 
 btn.addEventListener('click', reveal)
